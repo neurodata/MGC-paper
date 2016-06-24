09b5c599fff5234ad288bd4abef64213d169defd
cshen6 <cshen6@jhu.edu>
2016-06-24 17:51:52 -0400

    figure 1

--- Code/Plots/plot_auxiliary.m
+++ Code/Plots/plot_auxiliary.m
@@ -32,30 +32,22 @@ repp=1;
 fontSize=20;
 mkSize=30;
 
-cmap=zeros(3,3);
+cmap=zeros(4,3);
 gr = [0.5,0.5,0.5];
 ma = [1,0,1];
 % cy = [0,1,1];
 cmap(1,:) = ma;
 cmap(2,:) = ma;
+cmap(3,:) = ma;
+cmap(4,:) = ma;
 % cmap(3,:) = cy;
+map0=gr;
 map1=cmap;
 map2 = brewermap(128,'PRGn'); % brewmap
 map3 = brewermap(128,'Greens'); % brewmap
 map4 = brewermap(128,'GnBu'); % brewmap
 set(groot,'defaultAxesColorOrder',map1);
 s=3;t=4;
-
-% Generate data
-[x, y]=CorrSampleGenerator(type,n,dim,1, noise);
-if noise~=0
-    [x1, y1]=CorrSampleGenerator(type,10*n,dim,1, 0); % Plot 10*n points without noise to highlight the underlying dependency
-end
-% Get distance matrix
-[x,ind]=sort(x,'ascend');
-y=y(ind);
-C=squareform(pdist(x));
-D=squareform(pdist(y));
 
 % optimal scale
 tA=zeros(n,n,rep);
@@ -87,6 +79,19 @@ power1=pa;
 power1(1,:)=0;power1(:,1)=0; % Set the powers of all local tests at rank 0 to 0
 neighbor=maxNeighbors(power1,0,tN,tA);
 
+figure('units','normalized','position',[0 0 1 1])
+%%% Col 1
+% Generate data
+[x, y]=CorrSampleGenerator(type,n,dim,1, noise);
+if noise~=0
+    [x1, y1]=CorrSampleGenerator(type,10*n,dim,1, 0); % Plot 10*n points without noise to highlight the underlying dependency
+end
+% Get distance matrix
+[x,ind]=sort(x,'ascend');
+y=y(ind);
+C=squareform(pdist(x));
+D=squareform(pdist(y));
+
 % Permutation p-value
 tA=LocalCorr(C,D,2);
 tN=zeros(rep,n,n);
@@ -113,10 +118,8 @@ p=tN(:,k,l);
 p=tN(:,n,n);
 [f,xi]=ksdensity(p,'support',[-1,1]);
 
-
-figure('units','normalized','position',[0 0 1 1])
-%%% Col 1
-subplot(s,t,1);
+ax=subplot(s,t,1);
+set(groot,'defaultAxesColorOrder',map0);
 if noise==0
     plot(x,y,'.','MarkerSize',mkSize);
 else
@@ -125,11 +128,16 @@ end
 %xlabel('X');
 %ylabel('Y','Rotation',0,'position',[-1.4,-0.1]);
 
-    xlim([-1.2,1.2]);
-    ylim([-1.2,1.2]);
+xlim([-1.2,1.2]);
+ylim([-1.2,1.2]);
 
 title('Scatter Plot of (X,Y)')
 set(gca,'XTick',[],'YTick',[],'FontSize',fontSize); % Remove x axis tick
+pos = get(ax,'position');
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
+
 
 %%% Col 2
 ax=subplot(s,t,2);
@@ -138,11 +146,13 @@ imagesc(C');
 colormap(ax,map3);
 caxis([0,maxC]);
 set(gca,'YDir','normal')
-set(gca,'FontSize',fontSize); % Remove y axis ticks
-xlabel('i')
-ylabel('j')
-title('$\tilde{A}=\delta_{x}(x_{i},x_{j})$','interpreter','latex');
-pos = get(ax,'position');
+set(gca,'FontSize',13); % Remove y axis ticks
+xlabel('i','FontSize',fontSize,'position',[-5,0]);
+ylabel('j','Rotation',0,'position',[-7,25],'FontSize',fontSize);
+title('$\tilde{A}=\delta_{x}(x_{i},x_{j})$','interpreter','latex','FontSize',fontSize);
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 
 ax=subplot(s,t,3);
 imagesc(D');
@@ -150,10 +160,14 @@ set(gca,'FontSize',16)
 colormap(ax,map3);
 caxis([0,maxC]);
 set(gca,'YDir','normal')
-set(gca,'XTick',[],'YTick',[],'FontSize',fontSize); % Remove y axis ticks
-xlabel('i')
-ylabel('j')
-title('$\tilde{B}=\delta_{y}(y_{i},y_{j})$','interpreter','latex');
+set(gca,'FontSize',13); % Remove y axis ticks
+%set(gca,'XTick',[],'YTick',[],'FontSize',fontSize); % Remove y axis ticks
+xlabel('i','FontSize',fontSize,'position',[-5,0]);
+ylabel('j','Rotation',0,'position',[-7,25],'FontSize',fontSize);
+title('$\tilde{B}=\delta_{y}(y_{i},y_{j})$','interpreter','latex','FontSize',fontSize);
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 
 %Centering
 RC=disToRanks(C);
@@ -198,6 +212,9 @@ colormap(ax,map2)
 caxis([minC,maxC]);
 set(gca,'XTick',[],'YTick',[],'FontSize',fontSize);
 title('$$A$$','FontSize',fontSize,'interpreter','latex');
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 
 ax=subplot(s,t,7);
 minD=floor(min(min([A,B]))*10)/10;maxD=ceil(max(max([A,B]))*10)/10;
@@ -209,6 +226,9 @@ colormap(ax,map2)
 caxis([minD,maxD]);
 set(gca,'XTick',[],'YTick',[],'FontSize',fontSize); % Remove y axis ticks
 title('$$B$$','interpreter','latex');
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 
 % Global to Local
 RC=(RC>k);
@@ -227,6 +247,9 @@ caxis([minC,maxC]);
 set(gca,'YDir','normal')
 title('$$A^{k^{*}}$$','interpreter','latex');
 set(gca,'XTick',[],'YTick',[],'FontSize',fontSize); % Remove y axis ticks
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 
 ax=subplot(s,t,10);
 imagesc(B');
@@ -236,6 +259,9 @@ caxis([minD,maxD]);
 set(gca,'YDir','normal')
 title('$$B^{l^{*}}$$','interpreter','latex');
 set(gca,'XTick',[],'YTick',[],'FontSize',fontSize); % Remove y axis ticks
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 
 %Col3 Center
 ax=subplot(s,t,4);
@@ -249,9 +275,9 @@ colorbar('Ticks',[mH,0,MH]);
 caxis([mH,MH]);
 set(gca,'XTick',[],'YTick',[],'FontSize',fontSize); % Remove y axis ticks
 title('$$A.*B$$','FontSize',fontSize,'interpreter','latex');
-% pos2 = get(ax,'position');
-% pos2(3:4) = [pos(3:4)];
-% set(ax,'position',pos2);
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 
 %Col4 Center
 ax=subplot(s,t,8);
@@ -262,60 +288,70 @@ colormap(ax,map2)
 caxis([mH,MH]);
 set(gca,'XTick',[],'YTick',[],'FontSize',fontSize); % Remove y axis ticks
 title('$$A^{k^{*}}.*B^{l^{*}}$$','interpreter','latex');
-
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 % Col5
+set(groot,'defaultAxesColorOrder',map1);
 ax=subplot(s,t,12);
 kmin=2;
 hold on
 ph=power1(kmin:n,kmin:n)';
 imagesc(ph);
-set(gca,'FontSize',16)
+set(gca,'FontSize',13)
 set(gca,'YDir','normal')
 cmap=map4;
 colormap(ax,cmap)
-caxis([0.1 1])
-xlabel('Number Of Neighbors for X','FontSize',13)
-ylabel('Number Of Neighbors for Y','FontSize',13) %,'Rotation',0,'position',[-7,20]);
-xlim([1 n-1]);
-ylim([1 n-1]);
+caxis([floor(min(min(ph))*10)/10 1])
 h=colorbar('Ticks',[0.1,1]);
 set(h,'FontSize',fontSize);
+set(gca,'XTick',[10,20,30,40],'YTick',[10,20,30,40]);
+xlabel('Number Of Neighbors for X','FontSize',16)
+ylabel('Number Of Neighbors for Y','FontSize',16) %,'Rotation',0,'position',[-7,20]);
+xlim([1 n-1]);
+ylim([1 n-1]);
 
-% pos2 = get(ax,'position');
-% pos2(3:4) = [pos(3:4)];
-% set(ax,'position',pos2);
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 
 %     imagesc(k,l,1);
 hold off
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 %     %set(h,'FontSize',16,'location','southoutside');
 %set(gca,'XTick',[]); % Remove x axis ticks
 %set(gca,'YTick',[]); % Remove y axis ticks
-title('Multiscale Power Map','FontSize',fontSize);
+title('Multiscale Power Map','FontSize',16);
 colorbar
 
 % Col 5 p-value
 subplot(s,t,5)
 hold on
-plot(xi,f,'.:',xi1,f1,'.-','LineWidth',3);
+plot(xi,f,'.:',xi1,f1,'.-','LineWidth',2);
 set(gca,'FontSize',15);
 plot(tA(end),0.1,'.','MarkerSize',mkSize);
 plot(tA(k,l),0.1,'*','MarkerSize',10);
 set(gca,'XTick',[0,tA(k,l)],'YTick',[]); % Remove x axis ticks
 
 x1 = tA(end);
-y1 = 0.01;
+ind=find(xi>x1,1,'first');
+y1 = f(ind)+8;
 x2 = tA(k,l);
-y2 = 0.01;
+y2 = 2;
 txt1 = strcat('Mcorr p = ',num2str(1-pAll(end)));
 txt2 = strcat('MGC p < 0.001');
-a=text(x1,y1,txt1,'VerticalAlignment','top','HorizontalAlignment','center');
-b=text(x2,y2,txt2,'VerticalAlignment','top','HorizontalAlignment','center');
-set(a,'FontSize',13);
-set(b,'FontSize',13);
+a=text(x1,y1,txt1,'VerticalAlignment','bottom','HorizontalAlignment','left');
+b=text(x2,y2,txt2,'VerticalAlignment','bottom','HorizontalAlignment','center');
+set(a,'FontSize',18);
+set(b,'FontSize',18);
 xlim([minp,maxp]);
-ylabel('Probability Density','FontSize',15);
-title('Test Statistics Distributions','FontSize',fontSize);
-
+ylabel('Density','FontSize',fontSize);
+title('Test Statistics Distributions','FontSize',16);
+pos2 = get(ax,'position');
+pos2(3:4) = [pos(3:4)];
+set(ax,'position',pos2);
 hold off
 1-pAll(end)
 1-pAll(k,l)
@@ -324,5 +360,5 @@ power1(k,l)
 
 %
 F.fname=strcat(pre2, 'A2');
-F.wh=[8 4]*2;
+F.wh=[8 5]*2;
 print_fig(gcf,F)

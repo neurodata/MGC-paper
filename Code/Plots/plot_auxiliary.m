be5abf56cf039a80ffe2cd428d245b26ad1a8f7c
jovo <jovo@jhu.edu>
2016-06-28 23:27:28 -0400

    updated fig 1

--- Code/Plots/plot_auxiliary.m
+++ Code/Plots/plot_auxiliary.m
@@ -4,7 +4,7 @@ function []=plot_auxiliary(newSim,type,n,noise)
 % CorrSimPlotsA(type,n,dim,noise,pre1);
 % Used to generate figure A in the files
 
-%%% File path searching
+%% % File path searching
 if nargin<1
     newSim=0;
 end
@@ -47,7 +47,7 @@ map2 = brewermap(128,'PiYG'); % brewmap
 map3 = map2(size(map2,1)/2+1:end,:);
 % map3 = brewermap(128,'Greens'); % brewmap
 map4 = brewermap(128,'GnBu'); % brewmap
-s=3;t=4;
+s=3;t=5;
 gr=map2(120,:);
 pu=map2(8,:);
 loca=[0,1,0];
@@ -61,7 +61,7 @@ figure(1), clf
 set(gcf,'units','normalized','position',[0 0 1 1])
 
 %%  Col 1
-ax=subplot(s,t,1);
+ax=subplot(s,t,t+1);
 set(groot,'defaultAxesColorOrder',map2);
 plot(x,y,'.','MarkerSize',mkSize,'Color',gray);
 % xlim([-1.2,1.2]);
@@ -99,7 +99,7 @@ pos2(3:4) = [pos(3:4)];
 set(ax,'position',pos2);
 axis('square')
 
-ax=subplot(s,t,3);
+ax=subplot(s,t,2*t+2);
 if sameBar~=1
     maxC=ceil(max(max(D))*10)/10;
 end
@@ -114,7 +114,8 @@ xlabel('i','FontSize',fontSize,'position',[-10,0]);
 ylabel('j','Rotation',0,'position',[-10,30],'FontSize',fontSize);
 set(gca,'XTick',[1,round(n/2),n]); % Remove x axis ticks
 set(gca,'YTick',[1,round(n/2),n]); % Remove x axis ticks
-title('$\tilde{B}=\delta_{y}(y_{i},y_{j})$','interpreter','latex','FontSize',fontSize);
+% title('$\tilde{B}=\delta_{y}(y_{i},y_{j})$','interpreter','latex','FontSize',fontSize);
+title('$\tilde{B}$','interpreter','latex','FontSize',fontSize);
 pos2 = get(ax,'position');
 pos2(3:4) = [pos(3:4)];
 set(ax,'position',pos2);
@@ -154,7 +155,7 @@ mcorrH=A.*B;
 %
 
 %% Col 3
-ax=subplot(s,t,6);
+ax=subplot(s,t,3);
 if sameBar==1
     minC=floor(min(min([A,B]))*10)/10;maxC=ceil(max(max([A,B]))*10)/10;
 else
@@ -173,7 +174,7 @@ pos2(3:4) = [pos(3:4)];
 set(ax,'position',pos2);
 axis('square')
 
-ax=subplot(s,t,7);
+ax=subplot(s,t,2*t+3);
 if sameBar==1
     minD=floor(min(min([A,B]))*10)/10;maxD=ceil(max(max([A,B]))*10)/10;
 else
@@ -203,7 +204,7 @@ end
 
 
 %% Col 4
-ax=subplot(s,t,5);
+ax=subplot(s,t,4);
 imagesc(A');
 colormap(ax,map2)
 caxis([minC,maxC]);
@@ -216,7 +217,7 @@ pos2(3:4) = [pos(3:4)];
 set(ax,'position',pos2);
 axis('square')
 
-ax=subplot(s,t,10);
+ax=subplot(s,t,2*t+4);
 imagesc(B');
 set(gca,'FontSize',fontSize)
 colormap(ax,map2)
@@ -230,7 +231,7 @@ set(ax,'position',pos2);
 axis('square')
 
 %% Col3 Center
-ax=subplot(s,t,4);
+ax=subplot(s,t,t+3);
 MH=ceil(max(max(mcorrH(2:end,2:end))));
 mH=floor(min(min(mcorrH(2:end,2:end))));
 mH=min(mH,-MH);MH=max(MH,-mH);
@@ -247,7 +248,7 @@ set(ax,'position',pos2);
 axis('square')
 
 %% %Col4 Center
-ax=subplot(s,t,11);
+ax=subplot(s,t,t+4);
 mcorrH=A.*B;
 % MH=ceil(max(max(mcorrH(2:end,2:end))));
 % mH=floor(min(min(mcorrH(2:end,2:end))));
@@ -265,7 +266,7 @@ axis('square')
 
 %% Col5 MPM
 set(groot,'defaultAxesColorOrder',map1);
-ax=subplot(s,t,12);
+ax=subplot(s,t,2*t+t);
 kmin=2;
 hold on
 ph=power1(kmin:n,kmin:n)';
@@ -301,19 +302,20 @@ axis('square')
 
 %% Col5 p-value map
 set(groot,'defaultAxesColorOrder',map1);
-ax=subplot(s,t,8);
+ax=subplot(s,t,t+t);
 kmin=2;
 hold on
 ph=pAll(2:end,2:end)';
-%ph(ph<eps)=eps;
-imagesc(ph); %log(ph)-min(log(ph(:))));
+ph(ph<=eps)=0.0005;
+imagesc(log(ph)); %log(ph)-min(log(ph(:))));
 set(gca,'FontSize',fontSize)
 set(gca,'YDir','normal')
 cmap=map4;
 colormap(ax,flipud(cmap));
 %ceil(max(max(ph))*10)/10
-caxis([0 0.2]);
-h=colorbar('Ticks',[0 0.05 0.2]);%,'location','westoutside');
+% caxis([0 1]);
+cticks=[0.001, 0.01, 0.1, 0.5];
+h=colorbar('Ticks',log(cticks),'TickLabels',cticks);%,'location','westoutside');
 set(h,'FontSize',fontSize);
 set(gca,'XTick',[1,round(n/2)-1,n-1],'YTick',[1,round(n/2)-1,n-1],'XTickLabel',[2,round(n/2),n],'YTickLabel',[2,round(n/2),n],'FontSize',16);
 %xlabel('# of Neighbors for X','FontSize',16)
@@ -338,7 +340,7 @@ title('Multiscale P-Value Map','FontSize',16);
 axis('square')
 
 %% Col 5 p-value
-subplot(s,t,9)
+subplot(s,t,t)
 minp=min([min(tN(:,n,n)),min(tN(:,k,l)),tA(k,l),tN(n,n)]);
 minp=floor(minp*10)/10;
 maxp=max([max(tN(:,n,n)),max(tN(:,k,l)),tA(k,l),tN(n,n)]);

c0c9d7242ceefcf22563eb6c040cc888b52225b6
cshen6 <cshen6@jhu.edu>
2016-06-29 10:16:03 -0400

    p-value heatmap to log scale and do not use c-axis; add power=alpha in tex

--- Code/Plots/plot_realData.m
+++ Code/Plots/plot_realData.m
@@ -98,10 +98,11 @@ filename=strcat(pre1,'CorrPermDistTestTypeMigrainxCCI.mat');
 load(filename);
 figure
 kmin=2;
-imagesc(p2All(kmin:end,kmin:end)');
+imagesc(log(p2All(kmin:end,kmin:end)'));
 set(gca,'YDir','normal')
 colormap(flipud(map2))
-caxis([0 0.1])
+cticks=[0.001, 0.01, 0.1, 0.5];
+h=colorbar('Ticks',log(cticks),'TickLabels',cticks);%,'location','westoutside');
 set(gca,'FontSize',24);
 %     if i==3
 [n1,n2]=size(p2All);
@@ -109,7 +110,7 @@ xlabel('# of Neighbors for X','FontSize',24);
 ylabel('# of Neighbors for Y','FontSize',24);
 set(gca,'XTick',[1,round(n1/2)-1,n1-1],'XTickLabel',[2,round(n1/2),n1]); % Remove x axis ticks
 set(gca,'YTick',[1,round(n2/2)-1,n2-1],'YTickLabel',[2,round(n2/2),n2]); % Remove x axis ticks
-h=colorbar('Ticks',[0,0.05,0.1]);%,'location','westoutside');
+%h=colorbar('Ticks',[0,0.05,0.1]);%,'location','westoutside');
 %     else
 %         set(gca,'XTick',[]); % Remove x axis ticks
 %         set(gca,'YTick',[]); % Remove y axis ticks

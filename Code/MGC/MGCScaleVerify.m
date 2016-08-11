1be333919ff6743603a26d0c7e7f9cc8c90b755c
cshen6 <cshen6@jhu.edu>
2016-08-10 22:53:38 -0400

    trial code sync

--- Code/MGC/MGCScaleVerify.m
+++ Code/MGC/MGCScaleVerify.m
@@ -19,6 +19,7 @@ else
     prc=delta:delta:100-delta;
     prc=[prc';pc;mean(mean(P<alpha))*100];
     prc=sort(prc,'ascend');
+%     prc=[mean(mean(P<alpha))*100];
     warning ('off','all');
     C1=MonotoneRegion(P,thres/10);
     C2=MonotoneRegion(P',thres/10);
@@ -28,13 +29,17 @@ else
     for j=1:length(prc)
         alpha=prctile(P(P<1), prc(j));
         CC=(C1==1) & (P<=alpha);
-        CC=BoundedRegion(CC,thres);
-        CC=BoundedRegion(CC',thres)';
-            try
-                CC=bwareafilt(CC,1);
-            catch
-                CC=zeros(m,n);
-            end
+%         CC=(P<=alpha);
+         [~, ~, ~, CC] = FindLargestRectangles(CC, [0 0 1],ceil(thres*[m,n]));
+%         CC=BoundedRegion(CC,thres);
+%         CC=BoundedRegion(CC',thres)';
+%             try
+%                 CC=bwareafilt(CC,1);
+%             catch
+%                 CC=zeros(m,n);
+%             end
+%             figure
+%             imagesc(CC)
         mm=sum(sum(CC))/(m-1)/(n-1);
         if mm>thres
             indAll=(CC==1);
@@ -44,34 +49,35 @@ else
     end
 end
 
-function CC2=BoundedRegion(CC,thres)
-[m,n]=size(CC);
-nn=ceil(thres*m);
-nn2=ceil(thres*n);
-CC2=(zeros(m,n)==1);
-i=2;
-while i<=m
-    is=max(2,i-nn);
-    ie=min(m,i+nn);
-    tmp=find(sum(CC(is:ie,:),1)<ie-is+1);
-    tmp=[1;tmp';n+1];
-    tmp2=diff(tmp);
-    ind=find(tmp2>=max(nn2*2,max(tmp2)));
-    i=i+1;
-    if ~isempty(ind)
-        for t=1:length(ind)
-            j=ind(t);
-            CC2(is:ie,tmp(j)+1:tmp(j+1)-1)=1;
-        end
-        i=ie+1;
-    end
-end
+% function CC2=BoundedRegion(CC,thres)
+% [m,n]=size(CC);
+% nn=ceil(thres*m);
+% nn2=ceil(thres*n);
+% CC2=(zeros(m,n)==1);
+% i=2;
+% while i<=m
+%     is=max(2,i-nn);
+%     ie=min(m,i+nn);
+%     tmp=find(sum(CC(is:ie,:),1)<ie-is+1);
+%     tmp=[1;tmp';n+1];
+%     tmp2=diff(tmp);
+%     ind=find(tmp2>=max(nn2*2));%,max(tmp2)));
+%     i=i+1;
+%     if ~isempty(ind)
+%         for t=1:length(ind)
+%             j=ind(t);
+%             CC2(is:ie,tmp(j)+1:tmp(j+1)-1)=1;
+%         end
+%         i=ie+1;
+%     end
+% end
 
 % 
 function pdiff=MonotoneRegion(P,thres)
 if nargin<2
     thres=0.002;
 end
+% thres=0.002;
 [m,n]=size(P);
 pdiff=zeros(m,n);
 for i=2:m

95133586d5f23d9851a3447f3fca584b21c17426
cshen6 <cshen6@jhu.edu>
2016-09-12 22:33:11 -0400

    edit slopegraph
    include sample \mgc for mcorr in slopegraph D)

--- Code/Plots/plot_simulation_slopegraph.m
+++ Code/Plots/plot_simulation_slopegraph.m
@@ -16,7 +16,7 @@ pre1=strcat(rootDir,'Data/Results/'); % The folder to locate data
 pre2=strcat(rootDir,'Figures/Fig');% The folder to save figures
 
 total=20;
-fontSize=20;
+fontSize=24;
 %% Set colors
 map1=zeros(7,3);
 gr = [0.5,0.5,0.5];
@@ -31,16 +31,15 @@ HHG   = [0.5,0.5,0.5];
 map1(1,:)=mante; map1(5,:)=mante; % The color for MGC{dcorr} and global dcorr.
 map1(2,:)=dcorr; map1(6,:)=dcorr; % The color for MGC{mcorr} and global mcorr.
 map1(3,:)=mcorr; map1(7,:)=mcorr; % The color for MGC{Mantel} and global Mantel.
-map1(4,:)=HHG; % The color for HHG
+map1(4,:)=mcorr; map1(8,:)=mcorr; % The color for MGC{Mantel} and global Mantel.
 set(groot,'defaultAxesColorOrder',map1);
-strL={'Global';'MGC'};
 
 figure('units','normalized','position',[0 0 1 1])
-s=2;t=3;
+s=2;t=4;
 ls{3}='-';
 ls{2}='--';
 ls{1}=':';
-ls{4}='--';
+ls{4}='-';
 
 AUC=zeros(8,20);
 %load data
@@ -53,13 +52,13 @@ for j=1:total
     AUC(4,j)=mean(powerMGCD);
     AUC(5,j)=mean(powerM);
     AUC(6,j)=mean(powerMGCM);
-    AUC(7,j)=mean(powerHHG);
-    AUC(8,j)=mean(powerHHG);
+    AUC(7,j)=mean(powerM);
+    AUC(8,j)=mean(powerMGC);
 end
 
 x=1:2;
-str={'A. Mantel';'B. Dcorr';'C. Mcorr'};
-for j=1:3
+str={'A. Mantel';'B. Dcorr';'C. Mcorr';'D. Mcorr'};
+for j=1:4
     subplot(s,t,j);
     hold on
     for i=6:19
@@ -91,19 +90,23 @@ for j=1:total
     AUC(4,j)=mean(powerMGCD);
     AUC(5,j)=mean(powerM);
     AUC(6,j)=mean(powerMGCM);
-    AUC(7,j)=mean(powerHHG);
-    AUC(8,j)=mean(powerHHG);
+    AUC(7,j)=mean(powerM);
+    AUC(8,j)=mean(powerMGC);
 end
 
-for j=1:3
-    subplot(s,t,3+j);
+strL={'Global';'MGC'};
+for j=1:4
+    subplot(s,t,4+j);
     hold on
     for i=6:19
         plot(x,AUC(2*j-1:2*j,i),ls{j},'LineWidth',1,'Color',map1(j,:));
     end
     plot(x,[mean(AUC(2*j-1,6:19)) mean(AUC(2*j,6:19))],ls{j},'LineWidth',10,'Color',map1(j,:));
     ylim([0,1]);
-    set(gca,'XTickLabel',strL,'XTick',[1.15,1.87],'YTickLabel',[0,1],'YTick',0:1,'FontSize',fontSize,'TickLength',[0 1]);
+    if j==4
+        strL={'Global';'Sample MGC'};
+    end
+    set(gca,'XTickLabel',strL,'XTick',[1.1,1.9],'YTickLabel',[0,1],'YTick',0:1,'FontSize',fontSize,'TickLength',[0 1]);
     if j==1
         ylabel('High-Dimensional','FontSize',fontSize);
     else
@@ -112,5 +115,5 @@ for j=1:3
     hold off
 end
 F.fname=strcat(pre2,'Slope');
-F.wh=[6 3]*2;
+F.wh=[8 3]*2;
 print_fig(gcf,F)

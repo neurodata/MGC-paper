41ac0c03a67d874bd90719ce84fc88a4ac52ab0b
cshen6 <cshen6@jhu.edu>
2017-01-25 11:00:38 -0500

    heatmap

--- Code/Plots/plot_simulation_heatmaps.m
+++ Code/Plots/plot_simulation_heatmaps.m
@@ -65,7 +65,7 @@ if strcmp(figNumber,'1DHeat')
             plot([Xmin(j),Xmax(j)],[Ymax(j),Ymax(j)],'g','linewidth',lw)
             plot(K(j)-1,L(j)-1,'go','markerSize',8,'linewidth',4)
         end
-        plot(nn-1,nn-1,'.','markerSize',24,'MarkerFaceColor',glob,'Color',glob)
+        %plot(nn-1,nn-1,'.','markerSize',24,'MarkerFaceColor',glob,'Color',glob)
         hold off
         xlim([1,nn-1]);
         ylim([1,nn-1]);
@@ -122,7 +122,7 @@ else
             plot([Xmin(j),Xmax(j)],[Ymax(j),Ymax(j)],'g','linewidth',lw)
             plot(K(j)-1,L(j)-1,'go','markerSize',8,'linewidth',4)
         end
-        plot(n-1,n-1,'.','markerSize',24,'MarkerFaceColor',glob,'Color',glob)
+        %plot(n-1,n-1,'.','markerSize',24,'MarkerFaceColor',glob,'Color',glob)
         hold off
         xlim([1,n-1]);
         ylim([1,n-1]);

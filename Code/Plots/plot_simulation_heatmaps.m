190b5a39514595468a3f563dc37c1928361bf66d
cshen6 <cshen6@jhu.edu>
2017-02-20 07:32:47 -0500

    sync overleaf

--- Code/Plots/plot_simulation_heatmaps.m
+++ Code/Plots/plot_simulation_heatmaps.m
@@ -1,4 +1,4 @@
-function []=plot_simulation_heatmaps(figNumber)
+function []=plot_simulation_heatmaps
 % Used to plot figure 1-8 used in tex. Run like
 
 % total is usually 20.
@@ -6,9 +6,6 @@ function []=plot_simulation_heatmaps(figNumber)
 % pre2 specifies the location to save pictures.
 
 %%% File path searching
-if nargin<1
-    figNumber='HDHeat';
-end
 fpath = mfilename('fullpath');
 fpath=strrep(fpath,'\','/');
 findex=strfind(fpath,'/');
@@ -26,122 +23,127 @@ figure('units','normalized','position',[0 0 1 1])
 s=4;
 t=5;
 lw=1;
-if strcmp(figNumber,'1DHeat')
-    nn=60;
-    load(strcat(pre1,'CorrIndTest1DHeat.mat'));
-    for j=1:total
-        filename=strcat(pre1,'CorrIndTestType',num2str(j),'N100Dim1.mat');
-        load(filename)
-        subplot(s,t,j)
-        titlechar=strcat(num2str(j),'.',{' '},CorrSimuTitle(j));
-        kmin=2;
-        thres=0.8;
-        % ind=[find(max(power2,[],1)>=thres,1) lim];
-        % lim=min(ind);
-        ind=find(numRange==nn);
-        if isempty(ind)
-            ind=1;
-        end
-        ph=powerMLocal(kmin:numRange(ind),kmin:numRange(ind),ind)';
-        tt=find(sum(ph,2)==0,1,'first');
-        if isempty(tt)==false && tt~=1;
-            ph(tt:end,:)=repmat(ph(tt-1,:),numRange(ind)-tt,1);
-        end
-        tt=find(sum(ph,1)==0,1,'first');
-        if isempty(tt)==false && tt~=1;
-            ph(:,tt:end)=repmat(ph(:,tt-1),1,numRange(ind)-tt);
-        end
-        hold on
-        imagesc(ph);
-        set(gca,'YDir','normal')
-        colormap(map2)
-        caxis([0 thres])
-        
-        if j<total
-           plot([Xmin(j),Xmin(j)],[Ymin(j),Ymax(j)],'g','linewidth',lw)
-            plot([Xmax(j),Xmax(j)],[Ymin(j),Ymax(j)],'g','linewidth',lw)
-            plot([Xmin(j),Xmax(j)],[Ymin(j),Ymin(j)],'g','linewidth',lw)
-            plot([Xmin(j),Xmax(j)],[Ymax(j),Ymax(j)],'g','linewidth',lw)
-            plot(K(j)-1,L(j)-1,'go','markerSize',8,'linewidth',4)
-        end
-        %plot(nn-1,nn-1,'.','markerSize',24,'MarkerFaceColor',glob,'Color',glob)
-        hold off
-        xlim([1,nn-1]);
-        ylim([1,nn-1]);
-        set(gca,'FontSize',14);
-        set(gca,'XTick',[1,round(nn/2)-1,nn-1],'XTickLabel',[2,round(nn/2),nn]); % Remove x axis ticks
-        set(gca,'YTick',[1,round(nn/2)-1,nn-1],'YTickLabel',[2,round(nn/2),nn]); % Remove x axis ticks
-        title(titlechar,'FontSize',14, ...
-            'Units', 'normalized','Position', [0 1.05], 'HorizontalAlignment', 'left')
-        if j~=1
-            set(gca,'XTick',[]); % Remove x axis ticks
-            set(gca,'YTick',[]); % Remove y axis ticks
-        end
-        axis('square')
+figNumber='1DHeat';
+nn=60;
+load(strcat(pre1,'CorrIndTest1DHeat.mat'));
+for j=1:total
+    filename=strcat(pre1,'CorrIndTestType',num2str(j),'N100Dim1.mat');
+    load(filename)
+    subplot(s,t,j)
+    titlechar=strcat(num2str(j),'.',{' '},CorrSimuTitle(j));
+    kmin=2;
+    thres=0.8;
+    % ind=[find(max(power2,[],1)>=thres,1) lim];
+    % lim=min(ind);
+    ind=find(numRange==nn);
+    if isempty(ind)
+        ind=1;
     end
-    xlabel('# X Neighbors','position',[-170 -12],'FontSize',24);
-    ylabel('# Y Neighbors','position',[-423 165],'FontSize',24);
-    
-    %colorbar
-    h=colorbar('Ticks',[0,thres/2,thres]);
-    set(h,'FontSize',14);
-    h=suptitle(strcat('One-Dimensional Multiscale Power Maps'));
-else
-    figNumber='HDHeat';
-    load(strcat(pre1,'CorrIndTestHDHeat.mat'));
-    for j=1:total
-        filename=strcat(pre1,'CorrIndTestDimType',num2str(j),'N100Dim.mat');
-        load(filename)
-        subplot(s,t,j)
-        titlechar=strcat(num2str(j),'.',{' '},CorrSimuTitle(j));
-        kmin=2;thres=0.5;
-        ind=find(max(powerMGCM,[],1)>=thres,1,'last');
-        if isempty(ind)
-            ind=1;
-        end
-        ph=powerMLocal(kmin:n,kmin:n,ind)';
-        tt=find(sum(ph,2)==0,1,'first');
-        if isempty(tt)==false && tt~=1;
-            ph(tt:end,:)=repmat(ph(tt-1,:),n-tt,1);
-        end
-        tt=find(sum(ph,1)==0,1,'first');
-        if isempty(tt)==false && tt~=1;
-            ph(:,tt:end)=repmat(ph(:,tt-1),1,n-tt);
-        end
-        hold on
-        imagesc(ph);
-        set(gca,'YDir','normal')
-        colormap(map2)
-        caxis([0 thres])
-        
-        if j<total
-            plot([Xmin(j),Xmin(j)],[Ymin(j),Ymax(j)],'g','linewidth',lw)
-            plot([Xmax(j),Xmax(j)],[Ymin(j),Ymax(j)],'g','linewidth',lw)
-            plot([Xmin(j),Xmax(j)],[Ymin(j),Ymin(j)],'g','linewidth',lw)
-            plot([Xmin(j),Xmax(j)],[Ymax(j),Ymax(j)],'g','linewidth',lw)
-            plot(K(j)-1,L(j)-1,'go','markerSize',8,'linewidth',4)
-        end
-        %plot(n-1,n-1,'.','markerSize',24,'MarkerFaceColor',glob,'Color',glob)
-        hold off
-        xlim([1,n-1]);
-        ylim([1,n-1]);
-        set(gca,'XTick',[1,round(n/2)-1,n-1],'XTickLabel',[2,round(n/2),n]); % Remove x axis ticks
-        set(gca,'YTick',[1,round(n/2)-1,n-1],'YTickLabel',[2,round(n/2),n]); % Remove x axis ticks
-        if j~=1
-            set(gca,'XTick',[]); % Remove x axis ticks
-            set(gca,'YTick',[]); % Remove y axis ticks
-        end
-        set(gca,'FontSize',14);
-        title(titlechar,'FontSize',14);
-        axis('square');
+    ph=powerMLocal(kmin:numRange(ind),kmin:numRange(ind),ind)';
+    tt=find(sum(ph,2)==0,1,'first');
+    if isempty(tt)==false && tt~=1;
+        ph(tt:end,:)=repmat(ph(tt-1,:),numRange(ind)-tt,1);
     end
-    xlabel('# X Neighbors','position',[-290 -20],'FontSize',24);
-    ylabel('# Y Neighbors','position',[-715 277],'FontSize',24);
+    tt=find(sum(ph,1)==0,1,'first');
+    if isempty(tt)==false && tt~=1;
+        ph(:,tt:end)=repmat(ph(:,tt-1),1,numRange(ind)-tt);
+    end
+    hold on
+    imagesc(ph);
+    set(gca,'YDir','normal')
+    colormap(map2)
+    caxis([0 thres])
     
-    h=colorbar('Ticks',[0,thres/2,thres]);
-    set(h,'FontSize',14);
-    h=suptitle(strcat('High-Dimensional Multiscale Power Maps'));
+    if j<total
+        plot([Xmin(j),Xmin(j)],[Ymin(j),Ymax(j)],'g','linewidth',lw)
+        plot([Xmax(j),Xmax(j)],[Ymin(j),Ymax(j)],'g','linewidth',lw)
+        plot([Xmin(j),Xmax(j)],[Ymin(j),Ymin(j)],'g','linewidth',lw)
+        plot([Xmin(j),Xmax(j)],[Ymax(j),Ymax(j)],'g','linewidth',lw)
+        plot(K(j)-1,L(j)-1,'go','markerSize',8,'linewidth',4)
+    end
+    %plot(nn-1,nn-1,'.','markerSize',24,'MarkerFaceColor',glob,'Color',glob)
+    hold off
+    xlim([1,nn-1]);
+    ylim([1,nn-1]);
+    set(gca,'FontSize',14);
+    set(gca,'XTick',[1,round(nn/2)-1,nn-1],'XTickLabel',[2,round(nn/2),nn]); % Remove x axis ticks
+    set(gca,'YTick',[1,round(nn/2)-1,nn-1],'YTickLabel',[2,round(nn/2),nn]); % Remove x axis ticks
+    title(titlechar,'FontSize',14, ...
+        'Units', 'normalized','Position', [0 1.05], 'HorizontalAlignment', 'left')
+    if j~=1
+        set(gca,'XTick',[]); % Remove x axis ticks
+        set(gca,'YTick',[]); % Remove y axis ticks
+    end
+    axis('square')
 end
+xlabel('# X Neighbors','position',[-170 -12],'FontSize',24);
+ylabel('# Y Neighbors','position',[-423 165],'FontSize',24);
+
+%colorbar
+h=colorbar('Ticks',[0,thres/2,thres]);
+set(h,'FontSize',14);
+h=suptitle(strcat('One-Dimensional Multiscale Power Maps'));
+set(h,'FontSize',26,'FontWeight','normal');
+%
+F.fname=strcat(pre2, figNumber);
+F.wh=[8 5]*2;
+print_fig(gcf,F)
+
+figure('units','normalized','position',[0 0 1 1])
+figNumber='HDHeat';
+load(strcat(pre1,'CorrIndTestHDHeat.mat'));
+for j=1:total
+    filename=strcat(pre1,'CorrIndTestDimType',num2str(j),'N100Dim.mat');
+    load(filename)
+    subplot(s,t,j)
+    titlechar=strcat(num2str(j),'.',{' '},CorrSimuTitle(j));
+    kmin=2;thres=0.5;
+    ind=find(max(powerMGCM,[],1)>=thres,1,'last');
+    if isempty(ind)
+        ind=1;
+    end
+    ph=powerMLocal(kmin:n,kmin:n,ind)';
+    tt=find(sum(ph,2)==0,1,'first');
+    if isempty(tt)==false && tt~=1;
+        ph(tt:end,:)=repmat(ph(tt-1,:),n-tt,1);
+    end
+    tt=find(sum(ph,1)==0,1,'first');
+    if isempty(tt)==false && tt~=1;
+        ph(:,tt:end)=repmat(ph(:,tt-1),1,n-tt);
+    end
+    hold on
+    imagesc(ph);
+    set(gca,'YDir','normal')
+    colormap(map2)
+    caxis([0 thres])
+    
+    if j<total
+        plot([Xmin(j),Xmin(j)],[Ymin(j),Ymax(j)],'g','linewidth',lw)
+        plot([Xmax(j),Xmax(j)],[Ymin(j),Ymax(j)],'g','linewidth',lw)
+        plot([Xmin(j),Xmax(j)],[Ymin(j),Ymin(j)],'g','linewidth',lw)
+        plot([Xmin(j),Xmax(j)],[Ymax(j),Ymax(j)],'g','linewidth',lw)
+        plot(K(j)-1,L(j)-1,'go','markerSize',8,'linewidth',4)
+    end
+    %plot(n-1,n-1,'.','markerSize',24,'MarkerFaceColor',glob,'Color',glob)
+    hold off
+    xlim([1,n-1]);
+    ylim([1,n-1]);
+    set(gca,'XTick',[1,round(n/2)-1,n-1],'XTickLabel',[2,round(n/2),n]); % Remove x axis ticks
+    set(gca,'YTick',[1,round(n/2)-1,n-1],'YTickLabel',[2,round(n/2),n]); % Remove x axis ticks
+    if j~=1
+        set(gca,'XTick',[]); % Remove x axis ticks
+        set(gca,'YTick',[]); % Remove y axis ticks
+    end
+    set(gca,'FontSize',14);
+    title(titlechar,'FontSize',14);
+    axis('square');
+end
+xlabel('# X Neighbors','position',[-290 -20],'FontSize',24);
+ylabel('# Y Neighbors','position',[-715 277],'FontSize',24);
+
+h=colorbar('Ticks',[0,thres/2,thres]);
+set(h,'FontSize',14);
+h=suptitle(strcat('High-Dimensional Multiscale Power Maps'));
 set(h,'FontSize',26,'FontWeight','normal');
 %
 F.fname=strcat(pre2, figNumber);
--- Code/Plots/plot_simulation_powerCompare.m
+++ Code/Plots/plot_simulation_powerCompare.m
@@ -1,4 +1,4 @@
-function []=plot_simulation_powerCompare(select)
+function []=plot_simulation_powerCompare
 % Used to plot figure 1-8 used in tex. Run like
 
 % total is usually 20.
@@ -15,15 +15,11 @@ pre1=strcat(rootDir,'Data/Results/'); % The folder to locate data
 pre2=strcat(rootDir,'Figures/Fig');% The folder to save figures
 
 total=20;
-if nargin<1
-    select=1;
-end
 fontSize=20;
 %% Set colors
 gr = [0.5,0.5,0.5];
 bl=[0,0,0];
 
-figure('units','normalized','position',[0 0 1 1])
 s=1;t=4;
 ls{3}='-';
 ls{2}='--';
@@ -33,9 +29,14 @@ ls{1}=':';
 AUC=zeros(8,total);
 % %load data
 cons=0;
-if select==0
+for select=0:1
+    figure('units','normalized','position',[0 0 1 1])
     for j=1:total
-        filename=strcat(pre1,'CorrIndTestType',num2str(j),'N100Dim1.mat');
+        if select==0
+            filename=strcat(pre1,'CorrIndTestType',num2str(j),'N100Dim1.mat');
+        else
+            filename=strcat(pre1,'CorrIndTestDimType',num2str(j),'N100Dim.mat');
+        end
         load(filename)
         AUC(1,j+cons)=mean(powerMGCM)-mean(powerP);
         AUC(2,j+cons)=mean(powerMGCM)-mean(powerD);
@@ -46,114 +47,102 @@ if select==0
         AUC(7,j+cons)=mean(powerMGC)-mean(powerM);
         AUC(8,j+cons)=mean(powerMGC)-mean(powerHHG);
     end
-else
-    for j=1:total
-        filename=strcat(pre1,'CorrIndTestDimType',num2str(j),'N100Dim.mat');
-        load(filename)
-        AUC(1,j+cons)=mean(powerMGCM)-mean(powerP);
-        AUC(2,j+cons)=mean(powerMGCM)-mean(powerD);
-        AUC(3,j+cons)=mean(powerMGCM)-mean(powerM);
-        AUC(4,j+cons)=mean(powerMGCM)-mean(powerHHG);
-        AUC(5,j+cons)=mean(powerMGC)-mean(powerP);
-        AUC(6,j+cons)=mean(powerMGC)-mean(powerD);
-        AUC(7,j+cons)=mean(powerMGC)-mean(powerM);
-        AUC(8,j+cons)=mean(powerMGC)-mean(powerHHG);
-    end
-end
-%
-str={'Mantel';'Dcorr';'Mcorr';'HHG'};
-%lw=3;
-%ss=[1,3,2,5,4,6,7,13,19,16,17,10,9,14,8,11,12,15,18,20];
-for j=1:length(str)
-    subplot(s,t,j);
-    hold on
-    pv1=AUC(j,:);
-    %pv1=pv1(ss);
-    ord1=0.05*(1:total);
-%     plot(pv2,ord1,'*','MarkerSize',8,'Color',gr);
     
-    pv2=AUC(j+4,:);
-    %pv2=pv2(ss);
-    plot(pv2,ord1,'.','MarkerSize',15,'Color',bl);
-        plot(pv2,ord1,'*','MarkerSize',8,'Color',bl);
-%     if j==1
-%         h=legend('Oracle','Sample');
-%         set(h,'FontSize',fontSize, 'Units', 'normalized','Position',[0.06,0.75,0,0]);
-%         legend boxoff
-%     end
-%     plot(pv1,ord1,'.','MarkerSize',15,'Color',gr);
-    
-    plot(zeros(21,1),0:0.05:1,'--','Color',gr,'LineWidth',2)
-    mm1=mean(pv1);
-    mm2=mean(pv2);
-    pos=0;
-%     plot(mm1,pos,'.','MarkerSize',30,'Color',gr);
-    plot(mm2,pos,'*','MarkerSize',13,'Color',bl);
-    plot(mm2,pos,'.','MarkerSize',24,'Color',bl);
-    
-%     a=text(mm1,pos+0.02,num2str(round(mm1*100)/100),'VerticalAlignment','bottom','HorizontalAlignment','left','Color',gr,'Interpreter','latex');
-%     set(a,'FontSize',fontSize);
-%     b=text(mm2,pos-0.03,num2str(round(mm2*100)/100),'VerticalAlignment','top','HorizontalAlignment','left','Color',bl,'Interpreter','latex');
-%     set(b,'FontSize',fontSize);
-    
-    axis('square');
-    hold off
-    xlim([-1,1]);
-    ylim([0,1]);
-    set(gca,'FontSize',fontSize);
-    set(gca,'XTick',[]); % Remove y axis ticks
-    %set(gca,'XTickLabel',[0],'XTick',[0],'FontSize',fontSize);
-    labels={-1,[],[],[],0,[],[],[],1};
-    set(gca,'XTickLabel',labels,'XTick',-1:0.25:1,'FontSize',fontSize);
-    yt=[1,5,10,15,20];
-    set(gca,'YTickLabel',yt,'YTick',0.05*yt,'FontSize',fontSize);
+    %
+    str={'Mantel';'Dcorr';'Mcorr';'HHG'};
+    %lw=3;
+    %ss=[1,3,2,5,4,6,7,13,19,16,17,10,9,14,8,11,12,15,18,20];
+    for j=1:length(str)
+        subplot(s,t,j);
+        hold on
+        pv1=AUC(j,:);
+        %pv1=pv1(ss);
+        ord1=0.05*(1:total);
+        %     plot(pv2,ord1,'*','MarkerSize',8,'Color',gr);
         
-    if j==1
-        xlabel('Power Difference','FontSize',fontSize,...
-            'Units', 'normalized','Position', [0.4, -0.18]);%, 'HorizontalAlignment', 'left')
-        ylabel('Simulation Type','FontSize',fontSize, ...
-            'Units', 'normalized','Position', [-0.18 0.38])%, 'HorizontalAlignment', 'left')
-        %set(gca,'XTickLabel',[0,1],'XTick',[0,1],'FontSize',fontSize);
-    else
-        set(gca,'YTick',[]); % Remove y axis ticks
-        %set(gca,'XTick',[]); % Remove y axis ticks
+        pv2=AUC(j+4,:);
+        %pv2=pv2(ss);
+        plot(pv2,ord1,'.','MarkerSize',20,'Color',bl);
+        %plot(pv2,ord1,'*','MarkerSize',8,'Color',bl);
+        %     if j==1
+        %         h=legend('Oracle','Sample');
+        %         set(h,'FontSize',fontSize, 'Units', 'normalized','Position',[0.06,0.75,0,0]);
+        %         legend boxoff
+        %     end
+        %     plot(pv1,ord1,'.','MarkerSize',15,'Color',gr);
+        
+        plot(zeros(21,1),0:0.05:1,'--','Color',gr,'LineWidth',2)
+        mm1=mean(pv1);
+        mm2=mean(pv2);
+        pos=0;
+        %     plot(mm1,pos,'.','MarkerSize',30,'Color',gr);
+        %plot(mm2,pos,'*','MarkerSize',13,'Color',bl);
+        plot(mm2,pos,'.','MarkerSize',30,'Color',bl);
+        
+        %     a=text(mm1,pos+0.02,num2str(round(mm1*100)/100),'VerticalAlignment','bottom','HorizontalAlignment','left','Color',gr,'Interpreter','latex');
+        %     set(a,'FontSize',fontSize);
+        %     b=text(mm2,pos-0.03,num2str(round(mm2*100)/100),'VerticalAlignment','top','HorizontalAlignment','left','Color',bl,'Interpreter','latex');
+        %     set(b,'FontSize',fontSize);
+        
+        axis('square');
+        hold off
+        xlim([-1,1]);
+        ylim([0,1]);
+        set(gca,'FontSize',fontSize);
+        set(gca,'XTick',[]); % Remove y axis ticks
+        %set(gca,'XTickLabel',[0],'XTick',[0],'FontSize',fontSize);
+        labels={-1,[],[],[],0,[],[],[],1};
+        set(gca,'XTickLabel',labels,'XTick',-1:0.25:1,'FontSize',fontSize);
+        yt=[1,5,10,15,20];
+        set(gca,'YTickLabel',yt,'YTick',0.05*yt,'FontSize',fontSize);
+        
+        if j==1
+            xlabel('Power Difference','FontSize',fontSize,...
+                'Units', 'normalized','Position', [0.4, -0.18]);%, 'HorizontalAlignment', 'left')
+            ylabel('Simulation Type','FontSize',fontSize, ...
+                'Units', 'normalized','Position', [-0.18 0.38])%, 'HorizontalAlignment', 'left')
+            %set(gca,'XTickLabel',[0,1],'XTick',[0,1],'FontSize',fontSize);
+        else
+            set(gca,'YTick',[]); % Remove y axis ticks
+            %set(gca,'XTick',[]); % Remove y axis ticks
+        end
+        %set(gca,'YTick',[]); % Remove y axis ticks
+        
+        %     switch j
+        %         case 1
+        %             tit=strcat('A. MGC - ',{' '}, str(j,:));
+        %         case 2
+        %             tit=strcat('B. MGC - ',{' '}, str(j,:));
+        %         case 3
+        %             tit=strcat('C. MGC - ',{' '}, str(j,:));
+        %         case 4
+        %             tit=strcat('D. MGC - ',{' '}, str(j,:));
+        %     end
+        switch j
+            case 1
+                tit=strcat('A.');
+            case 2
+                tit=strcat('B.');
+            case 3
+                tit=strcat('C.');
+            case 4
+                tit=strcat('D.');
+        end
+        
+        txt1=strcat(tit,' Power(MGC)');
+        txt2=strcat({'     '},'- Power(',str(j,:),')');
+        txt2=txt2{1};
+        title({txt1,txt2},'FontSize',fontSize, ...
+            'Units', 'normalized','Position', [0 1.05], 'HorizontalAlignment', 'left')
+        hold off
     end
-    %set(gca,'YTick',[]); % Remove y axis ticks
     
-%     switch j
-%         case 1
-%             tit=strcat('A. MGC - ',{' '}, str(j,:));
-%         case 2
-%             tit=strcat('B. MGC - ',{' '}, str(j,:));
-%         case 3
-%             tit=strcat('C. MGC - ',{' '}, str(j,:));
-%         case 4
-%             tit=strcat('D. MGC - ',{' '}, str(j,:));
-%     end
-    switch j
-        case 1
-            tit=strcat('A.');
-        case 2
-            tit=strcat('B.');
-        case 3
-            tit=strcat('C.');
-        case 4
-            tit=strcat('D.');
+    if select~=1
+        figNumber='1DPowerMGCM';
+    else
+        figNumber='HDPowerMGCM';
     end
-
-    txt1=strcat(tit,' Power(MGC)');
-    txt2=strcat({'     '},'- Power(',str(j,:),')');
-    txt2=txt2{1};
-    title({txt1,txt2},'FontSize',fontSize, ...
-        'Units', 'normalized','Position', [0 1.05], 'HorizontalAlignment', 'left')
-    hold off
+    F.fname=strcat(pre2,figNumber);
+    F.wh=[8 2.2]*2;
+    print_fig(gcf,F)
 end
-
-if select~=1
-    figNumber='1DPowerMGCM';
-else
-    figNumber='HDPowerMGCM';
-end
-F.fname=strcat(pre2,figNumber);
-F.wh=[8 2.2]*2;
-print_fig(gcf,F)

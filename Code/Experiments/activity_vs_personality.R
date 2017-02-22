385ea5c0a8b58de4bbd592d59cce42deda7c9e37
joshua vogelstein <jovo@jhu.edu>
2017-02-22 17:53:26 -0500

    Update activity_vs_personality.R

--- Code/Experiments/activity_vs_personality.R
+++ Code/Experiments/activity_vs_personality.R
@@ -1,55 +1,29 @@
 # youngser park (2013), called this function "CGP" for "connectome x genome x personality"
 # this code pre-processed the data for the activity vs personality test
+# more details can be found here: http://www.cis.jhu.edu/~parky/CGP/cgp.html
 
-library(XML)
-library(R.matlab)
+ dname <- "http://www.cis.jhu.edu/~parky/CGP/Data/time_series_cc200_20130402/"
+    urlfiles <- readHTMLTable(dname,skip.rows=1:2)[[1]]$V2
+    urlfiles <- urlfiles[!is.na(urlfiles)]
+    files <- paste0(dname,urlfiles)
+    (n <- length(files)) # 42
 
-dname
-<-
-"http://www.cis.jhu.edu/~parky/CGP/Data/time_series_cc200_20130402/"
-urlfiles
-<-
-readHTMLTable(dname,skip.rows=1:2)[[1]]$V2
-urlfiles
-<-
-urlfiles[!is.na(urlfiles)]
-files
-<-
-paste0(dname,urlfiles)
-(n
-<-
-length(files))
-# 42
-require(flexmix)
-coh
-<-
-ts
-<-
-spect
-<-
-spect_norm
-<-
-NULL
-for
-(i
-in
-1:length(files))
-{
-ts[[i]]
-<-
-readMat(files[i])[[1]]
-# 194 x 197
-spect[[i]]
-<-
-spectrum(t(ts[[i]]))$spec[6:40,]
-# 100 (freq) x 194
- => (bandpass) 35 x 194
-spect_norm[[i]]
-<-
-scale(spect[[i]],center=FALSE,scale=colSums(spect[[i]]))
-# normalization
-coh[[i]]
-<-
-KLdiv(spect_norm[[i]])
-# Delta: 194 x 194
-}
+    require(flexmix)
+    coh <- ts <- spect <- spect_norm <- NULL
+    for (i in 1:length(files)) {
+        ts[[i]] <- readMat(files[i])[[1]] # 194 x 197
+        spect[[i]] <- spectrum(t(ts[[i]]))$spec[6:40,] # 100 (freq) x 194 => (bandpass) 35 x 194
+        spect_norm[[i]] <- scale(spect[[i]],center=FALSE,scale=colSums(spect[[i]])) # normalization
+        coh[[i]] <- KLdiv(spect_norm[[i]]) # Delta: 194 x 194
+    }
+
+    dissmat <- matrix(0,n,n)
+    for (i in 1:(n-1)) {
+        for (j in (i+1):n) {
+            dissmat[i,j] <- helldist(coh[[i]],coh[[j]]) # hellinger distance
+        }
+    }
+    dissmat <- dissmat + t(dissmat)
+    diag(dissmat) <- 0
+
+distC <- normalize(dissmat)

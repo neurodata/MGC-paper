a9075f75832ebe4d7725363075e6e784419b86fe
cshen6 <cshen6@jhu.edu>
2017-07-17 22:07:25 -0400

    r code demo push

--- Code/R/run_realData.R
+++ Code/R/run_realData.R
@@ -1,35 +1,34 @@
-# load the data
+# load the Brain vs Personality data and do a trial run
 
 run_realData <- function(){
-load("../../Data/Preprocessed/BrainHippoShape.RData")
+  library(ecodist)
+  library(energy)
+  library(HHG)
+  
+#load("../../Data/Preprocessed/BrainHippoShape.RData")
 ### Compute all local corr
-library(ecodist)
-library(energy)
-library(HHG)
-source("MGCLocalCorr.R")
-C=LMRS;P=as.matrix(dist(Label+0.01*runif(n)));
-ldcorr=MGCLocalCorr(C,P,option='dcor')$corr;
-lmdcorr=MGCLocalCorr(C,P,option='mcor')$corr;
-lmantel=MGCLocalCorr(C,P,option='mantel')$corr;
+#source("MGCLocalCorr.R")
+#C=LMRS;P=as.matrix(dist(Label+0.01*runif(n)));
+#ldcorr=MGCLocalCorr(C,P,option='dcor')$corr;
+#lmdcorr=MGCLocalCorr(C,P,option='mcor')$corr;
+#lmantel=MGCLocalCorr(C,P,option='mantel')$corr;
 ### Global corr from the original authors. 
 ### Their p-values in permutation test should be very similar to our local corr at largest scale,
 ### but the actual statistic can be slightly different.
 # global dcorr
-dcorr=dcor(as.dist(C),as.dist(P));
+#dcorr=dcor(as.dist(C),as.dist(P));
 # global mcorr
-mdcorr=dcor.ttest(C,P,distance=TRUE);
+#mdcorr=dcor.ttest(C,P,distance=TRUE);
 # Mantel test
-mantel=mantel(as.dist(C)~as.dist(P),nperm=1000);
+#mantel=mantel(as.dist(C)~as.dist(P),nperm=1000);
 # HHG test
-hhgr=hhg.test(C,P,nr.perm=1000);
+#hhgr=hhg.test(C,P,nr.perm=1000);
 
 
 ### Permutation Test of local corr
-source("MGCPermutationTest.R")
-C=LMRS;P=as.matrix(dist(Label+0.01*runif(n)));
-test=MGCPermutationTest(C,P,rep=1000,option='mcor')
-
-
+#source("MGCPermutationTest.R")
+#C=LMRS;P=as.matrix(dist(Label+0.01*runif(n)));
+#test=MGCPermutationTest(C,P,rep=1000,option='mcor')
 
 load("../../Data/Preprocessed/BrainCPData.RData")
 source("MGCLocalCorr.R")
@@ -51,4 +50,5 @@ source("MGCSampleStat.R")
 test=MGCSampleStat(C,P)
 source("MGCPermutationTest.R")
 test=MGCPermutationTest(C,P,rep=1000,option='mcor')
+return(test);
 }

2d9b61e560677463e0e83bf54a7286ff20e3509c
joshua vogelstein <jovo@jhu.edu>
2017-07-17 23:14:05 -0400

    Create run_demo.m

---
+++ Code/Experiments/run_demo.m
@@ -0,0 +1,17 @@
+%% simulate data
+type=6;
+d=1;
+noise=0;
+n=100;
+
+[x,y]=CorrSampleGenerator(type,n,d,0, noise);
+
+
+%% run mgc
+rep=10;
+option='mcor';
+
+A=squareform(pdist(x));
+B=squareform(pdist(y));
+
+[pMGC,statMGC,pLocalCorr,localCorr,optimalInd]=MGCPermutationTest(A,B,rep,option);
